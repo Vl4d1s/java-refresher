7f4952c2b106f853c0a42564cda7eb06db2704cd
Vladis Markin <vladismarkin@gmail.com>
2021-08-09 09:42:08 +0300

    Creating the Dog Class

---
+++ OOP_Part1/Inheritance/src/Dog.java
@@ -0,0 +1,14 @@
+public class Dog extends Animal {
+
+    private int eyes;
+    private int legs;
+    private int tail;
+    private int teeth;
+    private String coat;
+
+    public Dog(String name, int brain, int body, int size, int weight) {
+        super(name, brain, body, size, weight);
+    }
+
+
+}
